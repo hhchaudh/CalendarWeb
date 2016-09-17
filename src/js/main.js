e7883b95a62bd97315dbc441385b06314da54474
Sam Lopez <slopez1029@yahoo.com>
2016-09-16 21:05:15 -0500

    editing of layouts
    re-editing of week and year layouts

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,51 @@ function eraseText() {
 }
 
 function saveText() {
-    $("#output").val("");
+    localStorage.setItem("lastname", "Smith");
+    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+}
+
+function dataSave()
+{
+    var user = document.getElementById("user").value ;
+    //localStorage["user"] = user ;
+    localStorage.setItem("user", user) ;
+    alert("gmail id saved") ;
+    console.log("gmail id saved")
+}
+
+function dataLoad()
+{
+    var user = localStorage["user"] ;
+    document.getElementById("user").value = user ;
+}
+
+function passString(month){
+    window.localStorage.setItem("option", month);
+}
+
+function displayString()
+{
+    var month = window.localStorage.getItem("option").value;
+    console.log(month);
+}
+
+$("#btn_a").on("click", function() {
+    alert("a is clicked");
+});
+
+
+$("#btn_b").on("click", function() {
+    return doSomething(p,q,null);
+});
+
+
+function doSomething(p,q,r){
+    if(r != null){
+alert("r is not null");
+    } else {
+        alert("r is null");
+//do something for btn_b and return
+    }
+
 }
